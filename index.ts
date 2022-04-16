2377b71e03031f5c6891a7f33cada16f3ad18f24
Jack <oportojack@gmail.com>
2022-04-16 03:24:31 -0400

    Adds requested functionality
    NOTE: requires .env file with your own NASA_API_KEY, or replace with DEMO_KEY

--- index.ts
+++ index.ts
@@ -1,30 +1,44 @@
 /*
   Jack Oporto
   Alula junior dev interview task
-  https://www.alula.com/
 */
 
+//config
+require("dotenv").config();
 const axios = require("axios");
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 3000;
+const url =
+  "https://www.neowsapp.com/rest/v1/feed?start_date=2019-01-01&end_date=2019-01-07&detailed=false&api_key=" +
+  process.env.NASA_API_KEY;
+var total_asteroids = 0;
+var returned_asteroids = [];
 
-const targetData = {
-  dateStart: "2019-01-01",
-  dateEnd: "2019-01-07",
-  within: {
-    value: 9000000,
-    units: "kilometers",
-  },
-};
-
-//Axios get response
+//Get Nasa data
 axios
-  .get("https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=DEMO_KEY:id")
+  .get(url)
   .then((res) => {
-    console.log(`statusCode: ${res.status}`);
-    console.log(res);
+    //iterate for 7 days
+    for (let i = 0; i < 7; i++) {
+      // iterate for total count of asteroids on specific date
+      for (
+        let j = 0;
+        j < res.data.near_earth_objects["2019-01-0" + (i + 1)].length;
+        j++
+      ) {
+        //Add asteroid name to array if miss_distance > 9,000,000
+        if (
+          res.data.near_earth_objects["2019-01-0" + (i + 1)][j]
+            .close_approach_data[0].miss_distance.kilometers > 9000000
+        ) {
+          returned_asteroids.push(
+            res.data.near_earth_objects["2019-01-0" + (i + 1)][j].name
+          );
+        }
+      }
+    }
+
+    //print asteroid names
+    console.log(returned_asteroids);
   })
   .catch((error) => {
-    console.error(error);
+    console.log("An error occurred: ", error);
   });
